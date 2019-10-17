e9ca9d3312298607c3b6241a9fcc332e4ff315cb
SeaMinHo <55964213+SeaMinHo@users.noreply.github.com>
2019-10-17 09:27:45 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 // For command line git Chapter3
 // GitHub modification
 
-print("Hello world") 
-print("Tell your world")
-print("Tell his world")
-print("Tell her world")
+print("Hello ") 
+print("1")
+print("2")
+
